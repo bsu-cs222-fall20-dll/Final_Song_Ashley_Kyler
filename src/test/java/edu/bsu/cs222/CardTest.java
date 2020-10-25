57a10878a67f2011b4c2417f193fb1a7915d26f8
ltltnlt <70453427+ltltnlt@users.noreply.github.com>
2020-10-25 09:46:45 +0800

    Add 5 tests for Card class

--- src/test/java/edu/bsu/cs222/CardTest.java
+++ src/test/java/edu/bsu/cs222/CardTest.java
@@ -1,4 +1,38 @@
 package edu.bsu.cs222;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CardTest {
+    @Test
+    public void testTransformCardJNum(){
+        Card card = new Card();
+        Assertions.assertEquals(11, card.transformCardPoint("J"));
+    }
+
+    @Test
+    public void testTransformCardQNum(){
+        Card card = new Card();
+        Assertions.assertEquals(11, card.transformCardPoint("Q"));
+    }
+
+    @Test
+    public void testTransformCardKNum(){
+        Card card = new Card();
+        Assertions.assertEquals(11, card.transformCardPoint("K"));
+    }
+
+    @Test
+    public void testTransformCardANum(){
+        Card card = new Card();
+        Assertions.assertEquals(1, card.transformCardPoint("A"));
+    }
+
+    @Test
+    public void testCompareCardPoints(){
+        Card card = new Card();
+        Boolean result = card.compareCardPoints(1,11);
+        Assertions.assertTrue(result);
+    }
 }
