787015feda8d27113884175237ed2d3bfa8e0ee1
ltltnlt <70453427+ltltnlt@users.noreply.github.com>
2020-10-26 08:45:21 +0800

    Solved the tests problem and completed CardInitial and Card classes and their tests.

--- src/main/java/edu/bsu/cs222/Card.java
+++ src/main/java/edu/bsu/cs222/Card.java
@@ -1,46 +1,90 @@
 package edu.bsu.cs222;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 //The class is in the test process
 public class Card {
-    String suits[] = {"Heart", "Diamond", "Club", "Spade"};
-    String points[] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    static String suits[] = {"Heart", "Diamond", "Club", "Spade"};
+    static String cardNumbers[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    static final int totalCards = 52;//a set of new cards without joker
+    static CardInitial cards[] = new CardInitial[totalCards];
 
     //JSONObject json = JSONObject.fromObject(str);
     //Not sure if we need to use Json
 
-    public int transformCardPoint (String randomPoint){
-        // remember to replace the randomPoint with random method
-        if(randomPoint.equals("3")) {
-            return 3;
-        } else if(randomPoint.equals("4")){
-            return 4;
-        }else if(randomPoint.equals("5")){
-            return 5;
-        }else if(randomPoint.equals("6")){
-            return 6;
-        }else if(randomPoint.equals("7")){
-            return 7;
-        }else if(randomPoint.equals("8")){
-            return 8;
-        }else if(randomPoint.equals("9")){
-            return 9;
-        }else if(randomPoint.equals("10")){
-            return 10;
-        }else if(randomPoint.equals("A")) {
-            return 1;
-        }else{
-            return 11;
+    public Card() {
+        for (int i = 0; i < suits.length; i++) {
+            for (int j = 0; j < cardNumbers.length; j++) {
+                cards[i * 13 + j] = new CardInitial(suits[i], cardNumbers[j]);
+            }
         }
     }
 
-    public String compareCardPoints(int firstCardNum, int secondCardNum) {
-        //remember to replace the int
-        // with computer card and player card
-        if (firstCardNum > secondCardNum){
-            return "Bigger";
-        }else if(firstCardNum < secondCardNum){
-            return "Smaller";
-        }else {
-            return "Same";
+    public void shuffle() {
+        Random random = new Random();
+        for (int i = 0; i < cards.length; i++) {
+            int p = random.nextInt(cards.length);
+            CardInitial temp = cards[i];
+            cards[i] = cards[p];
+            cards[p] = temp;
         }
     }
+
+
+    public CardInitial[] getCard() {
+        return Card.cards;
+    }
+
+    public CardInitial getOneCard(int n) {
+        return Card.cards[n - 1];
+    }
+
+    public void displayCardsAfterShuffle() {
+        for (int i = 0; i < totalCards; i++) {
+            cards[i].displayNewCard();
+        }
+        System.out.println();
+    }
+
+    public String compareCardsAfterShuffle() {
+        CardInitial cardsBefore[] = new CardInitial[totalCards];
+        String result = new String();
+        for (int i = 0; i < totalCards; i++) {
+            if (cardsBefore[i] == cards[i]){
+                result = "T";
+            }else {
+                result = "F";
+            }
+        }
+        return result;
+    }
+
+    //reserve temporarily for Game
+    /*public String compareCardPoints(int firstCardNum, int secondCardNum) {
+        //remember to replace the int
+        // with computer card and player card
+        if (firstCardNum > secondCardNum) {
+            return "Bigger";
+        } else if (firstCardNum < secondCardNum) {
+            return "Smaller";
+        } else {
+            return "Same";
+        }
+    }*/
+
+    //old shuffle method
+    /*public void shuffle() {
+        for (int i = 0; i < suits.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                Card card = new Card();
+                card.setSuit(points[i]);
+                card.setPoint(suits[j]);
+                list.add(card);
+            }
+        }
+
+    }*/
 }
--- src/test/java/edu/bsu/cs222/CardTest.java
+++ src/test/java/edu/bsu/cs222/CardTest.java
@@ -3,56 +3,11 @@ package edu.bsu.cs222;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-//All failed aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+//Failed
 public class CardTest {
     @Test
-    public void testTransformCardJNum(){
+        public void testInitialCardClubJ(){
         Card card = new Card();
-        Assertions.assertEquals(11, card.transformCardPoint("J"));
-    }
-
-    @Test
-    public void testTransformCardQNum(){
-        Card card = new Card();
-        Assertions.assertEquals(11, card.transformCardPoint("Q"));
-    }
-
-    @Test
-    public void testTransformCardKNum(){
-        Card card = new Card();
-        Assertions.assertEquals(11, card.transformCardPoint("K"));
-    }
-
-    @Test
-    public void testTransformCardANum(){
-        Card card = new Card();
-        Assertions.assertEquals(1, card.transformCardPoint("A"));
-    }
-
-    @Test
-    public void testTransformCard10Num(){
-        Card card = new Card();
-        Assertions.assertEquals(10, card.transformCardPoint("10"));
-    }
-
-    @Test
-    public void testCompareCardPointsSmaller(){
-        Card card = new Card();
-        String result = card.compareCardPoints(1, 11);
-        Assertions.assertEquals("Smaller", result);
-    }
-
-    @Test
-    public void testCompareCardPointsBigger(){
-        Card card = new Card();
-        String result = card.compareCardPoints(11, 1);
-        Assertions.assertEquals("Bigger", result);
-    }
-
-    @Test
-    public void testCompareCardPointsSame(){
-        Card card = new Card();
-        String result = card.compareCardPoints(11, 11);
-        Assertions.assertEquals("Same", result);
+        Assertions.assertEquals("F", card.compareCardsAfterShuffle());
     }
 }
