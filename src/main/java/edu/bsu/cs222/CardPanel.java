c8d072d55c5f0af2602cc652f3362a5aa1213e20
ltltnlt <70453427+ltltnlt@users.noreply.github.com>
2020-11-25 08:48:52 +0800

    Create two classes for User Interface

--- src/main/java/edu/bsu/cs222/CardPanel.java
+++ src/main/java/edu/bsu/cs222/CardPanel.java
@@ -2,5 +2,6 @@ package edu.bsu.cs222;
 
 public class CardPanel {
     //create for card image
+    //it is divided from user interface
     Card card = new Card();
 }
--- src/main/java/edu/bsu/cs222/UserInterface.java
+++ src/main/java/edu/bsu/cs222/UserInterface.java
@@ -4,5 +4,7 @@ import javax.swing.*;
 
 public class UserInterface extends JFrame {
     //CardPanel cardPanel = new CardPanel();
+    //not sure how to finish the image work
+    //we may divide it into two parts
     JButton buttonStart = new JButton("Start");
 }
