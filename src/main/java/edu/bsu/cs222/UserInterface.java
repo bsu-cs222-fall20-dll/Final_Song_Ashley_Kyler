9d41d19770c3a607b9f22a53e43bc677c13da338
ltltnlt <70453427+ltltnlt@users.noreply.github.com>
2020-11-28 08:27:30 +0800

    Fix JAVAFX problem

--- src/main/java/edu/bsu/cs222/UserInterface.java
+++ src/main/java/edu/bsu/cs222/UserInterface.java
@@ -1,10 +1,31 @@
 package edu.bsu.cs222;
 
-import javax.swing.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-public class UserInterface extends JFrame {
+public class UserInterface extends Application {
     //CardPanel cardPanel = new CardPanel();
     //not sure how to finish the image work
     //we may divide it into two parts
-    JButton buttonStart = new JButton("Start");
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("21 Game");
+
+        Button buttonStart = new Button();
+        buttonStart.setText("Start");
+        Button buttonInstruction = new Button();
+        buttonInstruction.setText("Instruction");
+        Button buttonReportIssue = new Button();
+        buttonReportIssue.setText("Report Issue");
+
+        Scene scene = new Scene(stackPane, 300, 300);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
